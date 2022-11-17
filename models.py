edf4b89df27713aa22466f47e464d9296d6fb39f
Liliek Sidharta <sidhartaliliek@gmail.com>
2022-11-16 21:25:06 -0800

    Change bto ack_ref

--- models.py
+++ models.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
 
 class Venue(db.Model):
@@ -18,13 +17,22 @@ class Venue(db.Model):
   website_link = db.Column(db.String(100), nullable=True) 
   seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
   seeking_description = db.Column(db.String(500), nullable=True)
-  shows_venue = db.relationship('Show', back_populates='venue', lazy=True)
-  
-  def __repr__(self):
-    return f'id: {self.id}, name:{self.name}, city:{self.city}, state:{self.state}, address:{self.address}, \
-    phone:{self.phone}, genres: {self.genres}, image link: {self.image_link}, FB link: {self.facebook_link}, Website link:{self.website_link}, \
-    is seeking talents: {self.seeking_talent}, seeking desc: {self.seeking_description} '
+  shows = db.relationship('Show', backref='venue', lazy="joined", cascade="all, delete")
 
+  def create(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def __repr__(self):
+    return f'id: {self.id}, name:{self.name}, city:{self.city}, state:{self.state}, \
+      address:{self.address}, phone:{self.phone}, genres: {self.genres}, \
+      image link: {self.image_link}, FB link: {self.facebook_link}, \
+      Website link:{self.website_link}, is seeking talents: {self.seeking_talent}, \
+      seeking desc: {self.seeking_description} '   
 
 class Artist(db.Model):
   __tablename__ = 'artists'
@@ -42,22 +50,38 @@ class Artist(db.Model):
   website_link = db.Column(db.String(100), nullable=True)
   seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
   seeking_description = db.Column(db.String(500), nullable=True)
-  shows_artist = db.relationship('Show', back_populates='artist', lazy=True)
-  
-  def __repr__(self):
-    return f'<Artist id: {self.id}, name:{self.name}, city:{self.city}, state:{self.state}, address:{self.address}, \
-    phone:{self.phone}, genres: {self.genres}, image link: {self.image_link}, FB link: {self.facebook_link}, Website link:{self.website_link}, \
-    is seeking talents: {self.seeking_venue}, seeking desc: {self.seeking_description} >'
+  shows = db.relationship('Show', backref="artist", lazy="joined", cascade="all, delete")
 
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+  def create(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def __repr__(self):
+    return f'<Artist id: {self.id}, name:{self.name}, city:{self.city}, \
+      state:{self.state}, address:{self.address}, phone:{self.phone}, \
+      genres: {self.genres}, image link: {self.image_link}, FB link: \
+      {self.facebook_link}, Website link:{self.website_link}, \
+      is seeking talents: {self.seeking_venue}, seeking desc: \
+      {self.seeking_description} >'
+
 class Show(db.Model):
   __tablename__ = 'shows'
   id = db.Column(db.Integer, primary_key=True)
   venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
   artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
   start_time = db.Column(db.DateTime, nullable=False)
-  artist = db.relationship('Artist', back_populates='shows_artist')
-  venue = db.relationship('Venue', back_populates='shows_venue')
+  
+  def create(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
 
   def __repr__(self):
     return f'<Show id: {self.id}, start_time: {self.start_time}>'
