2fdbf9713339f96bf6f6050bc46a8203ddd072ac
Liliek Sidharta <sidhartaliliek@gmail.com>
2022-11-16 21:23:13 -0800

    Add sqlalchemy_track_modifications

--- config.py
+++ config.py
@@ -2,15 +2,14 @@ import os
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 # Enable debug mode.
 DEBUG = True
-
+SQLALCHEMY_ECHO = True
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 # Connect to the database
 DATABASE_NAME = 'fyyurapp'
 USERNAME = 'lieke'
 PASSWORD = 'Glen2865'
 HOST = 'localhost'
 PORT = '5432'
-# TODO IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://{}:{}@{}:{}/{}".format(USERNAME, PASSWORD, HOST, PORT, DATABASE_NAME)
